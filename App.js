f57ae0815078fe268f775d87d736a42e1d3b5ce3
awinder0230 <awinder0230@gmail.com>
2018-03-13 22:05:04 +0800

    init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import styles from './App.style';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
