980917f719cd5fc9b390656c0aa9a1a5cbbed834
Benjamin Tsai <bentsai@Benjamins-MacBook-Pro.local>
2018-04-01 14:14:32 +0800

    feature/0202 class menu update

--- src/reducers/classMenu.js
+++ src/reducers/classMenu.js
@@ -4,7 +4,13 @@ const initialState = {
 
 const reducerMap = {
   classList: {
-    get: (state, action) => ({ ...state, classMenu: { ...state.classMenu, classes: action.payload } }),
+    get: (state, action) => ({
+      ...state,
+      classMenu: {
+        ...state.classMenu,
+        classes: action.payload,
+      },
+    }),
     set: (state, action) => {
       if (action.payload) {
         return { ...state, classMenu: { ...state.classMenu, classes: action.payload } }
