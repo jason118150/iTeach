c03c803482d04f04cf800f22af5389e7073e167d
keevin60907 <b03203048@ntu.edu.tw>
2018-05-04 00:29:06 +0800

    add Touchbar

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {
-  Image,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native'
+import { View } from 'react-native'
 import PropTypes from 'prop-types'
 import CloseImage from '../../asset/close.png'
 import styles from './styles/Course.styles'
 import navAction from '../actions/nav.action'
 import QuizItem from '../components/QuizItem'
 import QuizItemData from '../components/QuizItemData'
+import Appbar from '../components/Appbar'
 
 const mapStateToProps = state => ({
   status: state.account.status,
@@ -31,14 +27,9 @@ class Quiz extends Component {
     const { quizItem } = this.props
     return (
       <View style={styles.container}>
-        <View style={styles.titleBar}>
-          <Text style={styles.title}>
-            {this.props.course}
-          </Text>
-          <TouchableHighlight style={styles.addSearchIconContainer} onPress={this.props.navAction.onExit} underlayColor='#3A8FB7'>
-            <Image style={styles.addSearchIcon} source={CloseImage} />
-          </TouchableHighlight>
-        </View>
+        <Appbar title={this.props.course.courseName}
+          rightIcon={CloseImage}
+          onRightPress={this.props.navAction.onExit}/>
         <View style={styles.itemContainer}>
           {QuizItemData.filter(item => item.user.includes(this.props.status))
             .map(item => (
@@ -60,7 +51,7 @@ Quiz.propTypes = {
     openDrawer: PropTypes.func.isRequired,
     onExit: PropTypes.func.isRequired,
   }).isRequired,
-  course: PropTypes.string.isRequired,
+  course: PropTypes.object.isRequired,
   quizItem: PropTypes.object,
   status: PropTypes.string.isRequired,
 }
